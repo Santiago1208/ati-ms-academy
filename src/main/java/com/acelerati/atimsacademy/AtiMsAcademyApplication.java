eb8813b2e390768ed185a6998fae0aa822663784
Santiago Restrepo <santiago.restrepop@perficient.com>
2023-06-23 05:26:56 -0500

    Add initial project structure
    - Set the server port
    - Add data source credentials
    - Add data source environment variables

---
+++ src/main/java/com/acelerati/atimsacademy/AtiMsAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.acelerati.atimsacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtiMsAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtiMsAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acelerati/atimsacademy/AtiMsAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acelerati.atimsacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtiMsAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
